9412f6893f8c13f092f91dc2c99ce71c4040c872
enabokov <edwardnabokov@gmail.com>
2018-12-11 01:17:47 +0200

    Implement light defense

--- internal/defense.py
+++ internal/defense.py
@@ -1,3 +1,4 @@
+import math
 from internal.configurable import Configurable
 
 
@@ -11,7 +12,7 @@ class Defense(Configurable):
         self.fire_time = 0
         self.target = None
 
-        if hasattr(self, 'images'):
+        if hasattr(self, "images"):
             self.image = self.images[0]
             self.rect = self.image.get_rect()
             self.rect.x = x
@@ -21,13 +22,54 @@ class Defense(Configurable):
             self.game.level.collision.block_rect(x, y, self.rect.width, self.rect.height)
 
     def update(self, delta):
-        raise NotImplemented
+        if self.attack == "none":
+            return
+
+        target = self.get_target()
+
+        self.fire_time += delta
+        while self.fire_time >= self.attack_rate:
+            self.fire_time -= self.attack_rate
+
+            if target is not None and target != self.rect.center:
+
+                # Create the flash (if specified).
+                if hasattr(self, "flash_offset"):
+                    self.game.explosions.add(DefenseLight(self.rect.center, target, self.flash_offset))
+
+                if self.attack_rate <= 0:
+                    self.kill()
+
+            if self.attack_rate <= 0:
+                break
+
+        if self.rotate:
+            center = self.rect.center
+
+            if self.target is None:
+                self.image = self.images[0]
+            else:
+                dx = self.target.rect.center[0] - center[0]
+                dy = self.target.rect.center[1] - center[1]
+                angle = math.degrees(math.atan2(-dy, dx))
+                if angle < 0:
+                    angle += 360
+
+                self.image = self.images[int(angle // 5)]
+
+            self.rect = self.image.get_rect()
+            self.rect.center = center
 
     def get_target(self):
         if self.target is not None and self.is_target_suitable(self.target):
             return self.target.rect.center
 
-        return
+        for t in self.game.wave.enemies:
+            if self.is_target_suitable(t):
+                self.target = t
+                return t.rect.center
+
+        return None
 
     def is_target_suitable(self, target):
         if target not in self.game.wave.enemies:
@@ -35,3 +77,17 @@ class Defense(Configurable):
 
         a, b = target.rect.center, self.rect.center
         return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 <= self.attack_range ** 2
+
+
+class DefenseLight(Configurable):
+    def __init__(self, defence_position, target, offset):
+        dx = target[0] - defence_position[0]
+        dy = target[1] - defence_position[1]
+        magnitude = math.sqrt(dx * dx + dy * dy)
+        dx *= (offset / magnitude)
+        dy *= (offset / magnitude)
+
+        super().__init__("DefenseFlash", defence_position[0] + dx - 16, defence_position[1] + dy - 16)
+
+    def update(self, delta):
+        super().update_animation(delta)
