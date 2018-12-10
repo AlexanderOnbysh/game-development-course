efa3e01911acebe0f7cbab31f78322f6200ffdb0
enabokov <edwardnabokov@gmail.com>
2018-12-10 23:56:45 +0200

    Implement interactive menu

--- internal/game.py
+++ internal/game.py
@@ -1,27 +1,36 @@
 import pygame
 
-from .interface.interface import Interface
-
 
 class Game:
     def __init__(self, window):
-        self.running = True
+        self.running = None
+
         self.window = window
         self.clock = pygame.time.Clock()
-        self.interface = Interface(self)
+        self.defences = pygame.sprite.Group()
+        self.bullets = pygame.sprite.Group()
+        self.explosions = pygame.sprite.Group()
 
     def run(self):
-        while self.running:
-            self.clock.tick(60)
+        self.running = True
 
-            # handle events
+        while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.quit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if not self.menu.visible:
+                        self.place_defence(pygame.mouse.get_pos())
+                    self.menu.clicked()
 
-            # redraw screen
+            self.menu.update()
             self.window.clear()
-            self.interface.draw(self.window.screen)
+            self.level.prefabs.draw(self.window.screen)
+            self.defences.draw(self.window.screen)
+            self.bullets.draw(self.window.screen)
+            self.wave.enemies.draw(self.window.screen)
+            self.explosions.draw(self.window.screen)
+            self.menu.draw(self.window.screen)
 
     def quit(self):
         self.running = False
