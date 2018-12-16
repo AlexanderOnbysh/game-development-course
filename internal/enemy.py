ae3cb21e7b58d4c4a3cccaa2f0a8985df89add20
Alexander Onbysh <onbysh.a@gmail.com>
2018-12-16 18:15:08 +0200

    add interaction with targets

--- internal/enemy.py
+++ internal/enemy.py
@@ -42,3 +42,32 @@ class Enemy(Configurable):
         self.rect.x = self.x
         self.rect.y = self.y
 
+    def reached_target(self):
+        if not self.path.done:
+
+            if self.target[0] < self.game.window.resolution[0] and self.path.points is not None and self.target in self.path.points:
+                self.path, self.target = self.game.level.pathfinding.get_partial_path(self.target)
+
+            return
+
+        self.target = self.path.next(self.target)
+        if not self.target:
+            self.game.level.lives -= 1
+            if (self.game.level.lives == 0):
+                self.game.menu.show_lose_screen()
+
+            self.kill()
+
+    def take_damage(self, damage):
+        self.health -= damage
+
+        if self.health <= 0:
+            self.kill()
+
+    def kill(self):
+        super().kill()
+
+        self.game.wave.enemy_killed()
+
+        if self.rect.x > 1:
+            self.game.level.money += self.money
